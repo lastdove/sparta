2dcf373dedfee8992c03116c88f024fb880675f4
lastdove <tpqms9917@gmail.com>
2024-08-27 22:11:27 +0900

    문자열을 정수로 바꾸기

--- sparta.java
+++ sparta.java
@@ -1,12 +1,11 @@
 class Solution {
-    public int[] solution(long n) {
-        String s = "" + n;
-        
-        int[] answer = new int[s.length()];
-        
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = (int) n % 10;
-            n/=10;
+    public int solution(String s) {
+        int answer = 0;
+        try {
+            answer = Integer.parseInt(s);
+        }
+        catch (NumberFormatException e) {
+            e.printStackTrace();
         }
         return answer;
     }
